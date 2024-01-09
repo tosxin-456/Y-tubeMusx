3bdf6c6d4469c2a591560988e42232aeed9027d7
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-09 15:40:02 +0100

    changed the design

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,17 +1,39 @@
+import React ,{useState} from 'react'
+import hamburger from '../assets/hamburger.svg'
+import close from '../assets/close.svg'
 import youTubeLogo from '../assets/logo.svg'
+import home from '../assets/home.svg'
 import '../css/navbar.css'
 
+
 const Navbar = () => {
+  
+  function iconChanger() {
+    const [icon, setIcon] = useState(hamburger);
+    function handleClick() {
+      if (icon === hamburger) {
+        setIcon(close);
+      } else {
+        setIcon(hamburger);
+      }
+    }
+}
+    
   return (
     <>
-      <header>
         <nav>
+          {/* <img className='hamburger' src={hamburger} alt="" id='hambe' /> */}
         <div className='image_and_text'>
             <img src={youTubeLogo} alt="" />
             <p>Music</p>
-       </div>
-        </nav>
-    </header>
+        </div>
+        <div className='options'>
+          <img src={home} alt="" />
+          <p>Home</p>
+        </div>
+      </nav>
+      <div className='menu'>
+      </div>
     </>
  )
 }
