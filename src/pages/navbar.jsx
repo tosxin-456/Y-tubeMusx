d2216ca934206027e0ed558fb716ea26c99dac1e
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-09 16:41:16 +0100

    navbar

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -3,6 +3,12 @@ import hamburger from '../assets/hamburger.svg'
 import close from '../assets/close.svg'
 import youTubeLogo from '../assets/logo.svg'
 import home from '../assets/home.svg'
+import playlist from '../assets/playlist.svg'
+import artist from '../assets/artist.svg'
+import premium from '../assets/premuim.svg'
+import liked from '../assets/liked.svg'
+import account from '../assets/my account.svg'
+import sixNine from '../assets/6ix9ine.svg'
 import '../css/navbar.css'
 
 
@@ -21,6 +27,7 @@ const Navbar = () => {
     
   return (
     <>
+      <section>
         <nav>
           {/* <img className='hamburger' src={hamburger} alt="" id='hambe' /> */}
         <div className='image_and_text'>
@@ -31,9 +38,28 @@ const Navbar = () => {
           <img src={home} alt="" />
           <p>Home</p>
         </div>
+        <div className='options'>
+          <img src={playlist} alt="" />
+          <p>Playlist</p>
+        </div>
+        <div className='options'>
+          <img src={artist} alt="" />
+          <p>Artist</p>
+        </div>
+        <div className='options'>
+          <img src={premium} alt="" />
+          <p>Premium</p>
+        </div>
+        <div className='options'>
+          <img src={liked} alt="" />
+          <p>Liked</p>
+        </div>   <div className='options'>
+          <img src={account} alt="" />
+          <p>Account</p>
+        </div>
       </nav>
-      <div className='menu'>
-      </div>
+        {/* <img src={sixNine} alt="" /> */}
+      </section>
     </>
  )
 }
