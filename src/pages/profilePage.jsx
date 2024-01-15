7f289532a2a536ad202b6801d3c4e4818c7e2886
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-15 12:52:55 +0100

    added the back button

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import logout from '../assets/majesticons_logout-half-circle.svg'
 import avatar from '../assets/avatar.svg'
 import '../css/profile.css'
+import back from '../assets/back.svg'
 import Navbar from "./navbar"
 import { Link } from "react-router-dom"
 
@@ -9,8 +10,12 @@ import { Link } from "react-router-dom"
 const Profile = () => {
   return (
     <>
-      <Navbar/>
       <div className="profileWrap">
+        <div className="back">
+          <Link to='/home'>
+           <img src={back} alt="" />
+          </Link>  
+        </div>
         <div className="profileHead">
         <h2>Profile</h2>
         <div className="profileImage">
@@ -19,10 +24,10 @@ const Profile = () => {
         </div>
         <div className="profileInfo">
         <p><span>Profile Name: </span>Tosin Poppins</p>
-        <p><span>ID: </span>Tosin Poppins</p>
-        <p><span>Email: </span>Tosin Poppins</p>
-        <p><span>Top Genre: </span>Tosin Poppins</p>
-        <p><span>Premium Membership: </span>Tosin Poppins</p>
+        <p><span>ID: </span>TP0909</p>
+        <p><span>Email: </span>tosinekshally@gmail.com</p>
+        <p><span>Top Genre: </span>Pop</p>
+        <p><span>Premium Membership: </span>true</p>
         </div>
         <div className="logOutIcon">
           <Link to='/'>
