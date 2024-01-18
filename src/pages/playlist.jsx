3c168a02a26675d0a3f7f126cdcd8310953555dd
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-18 21:36:27 +0100

    added an image

--- src/pages/playlist.jsx
+++ src/pages/playlist.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from '../components/navbar';
-
-
+import background1 from '../assets/phone taylor.jpg'
+import '../css/playlist.css'
 
 
 const Playlist = ()=> {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className='backgrounDiv'>
+      <img className='background' src={background1} alt="" />
+      </div>
+
     </div>
   )
 }
